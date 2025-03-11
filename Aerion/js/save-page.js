0d3d513d55d8d557c6eac31d2bb57e2b93a83d80
NikolaPetrow <161580616+NikolaPetrow@users.noreply.github.com>
2025-03-11 11:41:36 +0200

    Delete Aerion directory

--- Aerion/js/save-page.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const loginButton = document.querySelector('a[href="login.html"]');
-    if (loginButton) {
-        loginButton.addEventListener('click', function(e) {
-            sessionStorage.setItem('previousPage', window.location.href);
-        });
-    }
-}); 
